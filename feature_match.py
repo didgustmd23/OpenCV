7fa01824088e90ada4605e5650913b5b79423fc0
yhs <didgustmd23@naver.com>
2026-09-21 19:06:03 +0900

    Initial commit

---
+++ feature_match.py
@@ -0,0 +1,16 @@
+# 공통 함수
+# 이미지에서 특징점을 찾고, 각 특징점의 특징을 숫자로 표현
+def detect_features():
+    pass
+# 첫 번째 이미지의 특징점과 두 번째 이미지의 특징점을 서로 비교해서 비슷한 것끼리 연결
+def match_features():
+    pass
+# 애매하거나 잘못된 매칭을 제거
+def ratio_test():
+    pass
+# 실제로 두 사진의 같은 위치를 가리키는지 기하학적으로 검사
+def ransac_filter():
+    pass
+# 매칭 결과를 사람이 눈으로 확인할 수 있도록 이미지로 그림
+def draw_matches():
+    pass
